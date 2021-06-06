82f24eaa1c1d1c09b51f6e3e3878b41c7e60016b
Aleksandr <totamonik@gmail.com>
2021-06-06 21:04:11 +0300

    few upgrades

--- src/render.js
+++ src/render.js
@@ -1,16 +1,47 @@
 function Renderer(canvas) {
 	this.canvas = canvas;
-	this.scene = new Scene();
+    this.ctx = canvas.getContext('2d');
+	this.scene = new Scene('Main');
+    this.time = 0;
+    this.pause = false;
+
 	this.render = ()=>{
-		for (obj of this.scene.objects) {
-			this.scene.objects.sort((a, b)=>{
-				return a.zindex - b.zindex;
-			})
-			obj.render();
-		}
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        if (!this.pause) {
+            for (obj of this.scene.objects) {
+                this.scene.objects.sort((a, b)=>{
+                    return a.zindex - b.zindex;
+                });
+                obj.update();
+                obj.render(this.ctx);
+            }
+            if (this.scene.area)
+                this.scene.area.render(this.ctx);
+        }
+        requestAnimationFrame(this.render);
 	}
+
+    this.stop = ()=>{
+        this.pause = true;
+    }
+
+    this.start = ()=>{
+        this.pause = false;
+    }
+
+    this.render();
 }
 
-function Scene() {
-	
+function Scene(name) {
+    this.name = name;
+	this.objects = [];
+    this.area = null;
+
+    this.add = (obj)=>{
+        this.objects.push(obj);
+        this.objects.sort((a, b)=>{
+            return a.zindex - b.zindex;
+        });
+        console.log('Added object!');
+    }
 }
--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', start, false);
 
 
+
 function start() {
 	console.log('Starting new game...');
 	
@@ -15,9 +16,30 @@ function start() {
 	body.appendChild(canvas);
 	window.addEventListener('resize', resizeCanvas, false);
 	
-	
-	
+	const render = new Renderer(canvas);
+	const scene = render.scene;
+	scene.add(new BaseUnit(new Vector2(100, 100)));
+
 	console.log('Game started');
+
+	canvas.addEventListener('mousemove', (e)=>{
+		if (e.buttons==1 && e.button==0) {
+			if (scene.area) {
+				scene.area.x1 = e.offsetX;
+				scene.area.y1 = e.offsetY;
+			} else {
+				scene.area = new Area(e.offsetX, e.offsetY, e.offsetX, e.offsetY);
+			}
+		}
+		return false;
+	});
+
+	canvas.addEventListener('mouseup', (e)=>{
+		if (scene.area) {
+			scene.area = null;
+		}
+		return false;
+	});
 }
 
 function resizeCanvas() {
@@ -26,3 +48,31 @@ function resizeCanvas() {
 	canvas.height = window.innerHeight;
 }
 
+function BaseUnit(position = new Vector2(), width = 10, height = 10, zindex = 0) {
+	this.position = position;
+	this.width = width;
+	this.height = height;
+	this.zindex = zindex;
+	
+	this.update = ()=>{
+
+	}
+
+	this.render = (ctx)=>{
+		ctx.fillStyle = 'lime';
+		ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+	}
+}
+
+function Area(x0, y0, x1, y1) {
+	this.x0 = x0;
+	this.y0 = y0;
+	this.x1 = x1;
+	this.y1 = y1;
+
+	this.render = (ctx) => {
+		ctx.strokeStyle = 'lime';
+		ctx.strokeRect(this.x0, this.y0, this.x1 - this.x0, this.y1 - this.y0);
+	}
+
+}
